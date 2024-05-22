371747028761eb862e3e35e5758bc8ac7cdb9fa3
mrosa <mrosa@gvdasa.com.br>
2024-05-21 22:11:33 -0300

    Fix banner title

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -21,7 +21,6 @@ export const Title = styled.h2`
   text-align: center;
   font-weight: bold;
   width: 100%;
-  max-width: 80%;
   margin-top: 36px;
   margin-bottom: 36px;
 
@@ -45,12 +44,17 @@ export const BannerImg = styled.div`
 `
 
 export const Subtitle = styled.p`
- font-size: 40px;
- letter-spacing: .3px;
- font-weight: 400;
- color: #FFF;
- margin-top: 32px;
- text-align: center;
+  font-size: 24px;
+  letter-spacing: .3px;
+  font-weight: 400;
+  color: #FFF;
+  margin-top: 32px;
+  text-align: center;
+  line-height: 26px;
+
+ @media (min-width: 720px) {
+  font-size: 40px;
+ }
 
  span {
   color: #c70002;
@@ -59,10 +63,14 @@ export const Subtitle = styled.p`
 
 export const Tags = styled.p`
   letter-spacing: .3px;
-  font-size: 25px;
+  font-size: 19px;
   font-weight: 700;
   color: #C9C9C9;
   text-align: center;
+
+  @media (min-width: 720px) {
+    font-size: 25px;
+  }
 `
 
 export const BtnContainer = styled.div`
